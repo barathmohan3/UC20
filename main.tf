3f4398b66e8cf0e41032bd485214a5aba57b8e77
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-08 22:13:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "lambda" {
   runtime          = "python3.10"
   lambda_zip_path  = "lambda.zip"
   env_vars = {
-    TABLE_NAME =  module.dynamodb.table_name
+    TABLE_NAME = module.dynamodb.dynamodb_table_name 
     EMAIL_TO   = "barathmohansiva3@gmail.com"
   }
   source_arn = module.api_gateway.source_arn
