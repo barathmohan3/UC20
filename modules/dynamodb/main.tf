1978077330401ec8f4f34e5dccd32cd7178a1453
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-08 12:54:35 +0530

    Update main.tf

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,9 +1,9 @@
-resource "random_id" "iam_suffix" {
+resource "random_id" "dynamodb_suffix" {
   byte_length = 4
 }
 
 resource "aws_dynamodb_table" "contact_form" {
-  name           = var.table_name
+  name           = "${var.table_name}_${random_id.dynamodb_suffix.hex}"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
 
